ea132b873c676cd0b84950aeb541826beec1968e
ueki05 <uekiyuta1991@gmail.com>
2017-10-29 14:57:47 +0900

    generate:module frontend Hello, add World action in Hello module

---
+++ apps/frontend/modules/Hello/actions/actions.class.php
@@ -0,0 +1,26 @@
+<?php
+
+/**
+ * Hello actions.
+ *
+ * @package    sf14
+ * @subpackage Hello
+ * @author     Your name here
+ * @version    SVN: $Id: actions.class.php 23810 2009-11-12 11:07:44Z Kris.Wallsmith $
+ */
+class HelloActions extends sfActions
+{
+ /**
+  * Executes index action
+  *
+  * @param sfRequest $request A request object
+  */
+  public function executeIndex(sfWebRequest $request)
+  {
+    $this->forward('default', 'module');
+  }
+
+  public function executeWorld()
+  {
+  }
+}

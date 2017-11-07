dda866d33a79e6e6ca8b66934a58c9e326dd0438
ueki05 <uekiyuta1991@gmail.com>
2017-11-08 00:18:02 +0900

    4-6-2 InquiryFormクラスの実装(基本)

--- apps/frontend/modules/Inquiry/templates/newSuccess.php
+++ apps/frontend/modules/Inquiry/templates/newSuccess.php
@@ -1,2 +1,10 @@
-<h2>お問い合わせフォーム</h2>
-後で実装
+<?php slot('breadcrumb') ?>
+<?php echo link_to('Symfony楽団ホームページ', 'homepage') ?> >
+お問い合わせ
+<?php end_slot() ?>
+<?php echo $form->renderFormTag(url_for('inquiry_new')) ?>
+<table>
+<?php echo $form; ?>
+</table>
+<input type="submit" name="submit" value="送信" />
+</form>

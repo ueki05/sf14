b42fd3a6ca467bc5428a031b58d9bf641d883368
ueki05 <uekiyuta1991@gmail.com>
2018-04-29 18:40:17 +0900

    モデルクラスのビルド

---
+++ lib/filter/doctrine/base/BaseArticleFormFilter.class.php
@@ -0,0 +1,56 @@
+<?php
+
+/**
+ * Article filter form base class.
+ *
+ * @package    sf14
+ * @subpackage filter
+ * @author     Your name here
+ * @version    SVN: $Id: sfDoctrineFormFilterGeneratedTemplate.php 29570 2010-05-21 14:49:47Z Kris.Wallsmith $
+ */
+abstract class BaseArticleFormFilter extends BaseFormFilterDoctrine
+{
+  public function setup()
+  {
+    $this->setWidgets(array(
+      'title'        => new sfWidgetFormFilterInput(array('with_empty' => false)),
+      'body'         => new sfWidgetFormFilterInput(array('with_empty' => false)),
+      'published_at' => new sfWidgetFormFilterDate(array('from_date' => new sfWidgetFormDate(), 'to_date' => new sfWidgetFormDate())),
+      'created_at'   => new sfWidgetFormFilterDate(array('from_date' => new sfWidgetFormDate(), 'to_date' => new sfWidgetFormDate(), 'with_empty' => false)),
+      'updated_at'   => new sfWidgetFormFilterDate(array('from_date' => new sfWidgetFormDate(), 'to_date' => new sfWidgetFormDate(), 'with_empty' => false)),
+    ));
+
+    $this->setValidators(array(
+      'title'        => new sfValidatorPass(array('required' => false)),
+      'body'         => new sfValidatorPass(array('required' => false)),
+      'published_at' => new sfValidatorDateRange(array('required' => false, 'from_date' => new sfValidatorDateTime(array('required' => false, 'datetime_output' => 'Y-m-d 00:00:00')), 'to_date' => new sfValidatorDateTime(array('required' => false, 'datetime_output' => 'Y-m-d 23:59:59')))),
+      'created_at'   => new sfValidatorDateRange(array('required' => false, 'from_date' => new sfValidatorDateTime(array('required' => false, 'datetime_output' => 'Y-m-d 00:00:00')), 'to_date' => new sfValidatorDateTime(array('required' => false, 'datetime_output' => 'Y-m-d 23:59:59')))),
+      'updated_at'   => new sfValidatorDateRange(array('required' => false, 'from_date' => new sfValidatorDateTime(array('required' => false, 'datetime_output' => 'Y-m-d 00:00:00')), 'to_date' => new sfValidatorDateTime(array('required' => false, 'datetime_output' => 'Y-m-d 23:59:59')))),
+    ));
+
+    $this->widgetSchema->setNameFormat('article_filters[%s]');
+
+    $this->errorSchema = new sfValidatorErrorSchema($this->validatorSchema);
+
+    $this->setupInheritance();
+
+    parent::setup();
+  }
+
+  public function getModelName()
+  {
+    return 'Article';
+  }
+
+  public function getFields()
+  {
+    return array(
+      'id'           => 'Number',
+      'title'        => 'Text',
+      'body'         => 'Text',
+      'published_at' => 'Date',
+      'created_at'   => 'Date',
+      'updated_at'   => 'Date',
+    );
+  }
+}
---
+++ lib/form/doctrine/base/BaseArticleForm.class.php
@@ -0,0 +1,49 @@
+<?php
+
+/**
+ * Article form base class.
+ *
+ * @method Article getObject() Returns the current form's model object
+ *
+ * @package    sf14
+ * @subpackage form
+ * @author     Your name here
+ * @version    SVN: $Id: sfDoctrineFormGeneratedTemplate.php 29553 2010-05-20 14:33:00Z Kris.Wallsmith $
+ */
+abstract class BaseArticleForm extends BaseFormDoctrine
+{
+  public function setup()
+  {
+    $this->setWidgets(array(
+      'id'           => new sfWidgetFormInputHidden(),
+      'title'        => new sfWidgetFormInputText(),
+      'body'         => new sfWidgetFormTextarea(),
+      'published_at' => new sfWidgetFormDateTime(),
+      'created_at'   => new sfWidgetFormDateTime(),
+      'updated_at'   => new sfWidgetFormDateTime(),
+    ));
+
+    $this->setValidators(array(
+      'id'           => new sfValidatorChoice(array('choices' => array($this->getObject()->get('id')), 'empty_value' => $this->getObject()->get('id'), 'required' => false)),
+      'title'        => new sfValidatorString(array('max_length' => 255)),
+      'body'         => new sfValidatorString(array('max_length' => 3000)),
+      'published_at' => new sfValidatorDateTime(array('required' => false)),
+      'created_at'   => new sfValidatorDateTime(),
+      'updated_at'   => new sfValidatorDateTime(),
+    ));
+
+    $this->widgetSchema->setNameFormat('article[%s]');
+
+    $this->errorSchema = new sfValidatorErrorSchema($this->validatorSchema);
+
+    $this->setupInheritance();
+
+    parent::setup();
+  }
+
+  public function getModelName()
+  {
+    return 'Article';
+  }
+
+}
